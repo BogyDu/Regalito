625159f23d82a59de05a2be103f418f47b5f8148
Bogdan <85745111+BogyDu@users.noreply.github.com>
2024-05-29 00:06:16 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -2,46 +2,46 @@ const routine = {
     1: {
         day: "Día 1: Pecho y Tríceps",
         exercises: [
-            { name: "Press de banca con barra", series: "4", reps: "8-12" },
-            { name: "Press inclinado con mancuernas", series: "3", reps: "10-12" },
-            { name: "Fondos en paralelas", series: "3", reps: "8-10" },
-            { name: "Aperturas con mancuernas", series: "3", reps: "12" },
-            { name: "Press francés con barra", series: "3", reps: "10-12" },
-            { name: "Extensiones de tríceps con cable", series: "3", reps: "12" }
+            { name: "Press de banca con barra", series: 4, reps: "8-12" },
+            { name: "Press inclinado con mancuernas", series: 3, reps: "10-12" },
+            { name: "Fondos en paralelas", series: 3, reps: "8-10" },
+            { name: "Aperturas con mancuernas", series: 3, reps: 12 },
+            { name: "Press francés con barra", series: 3, reps: "10-12" },
+            { name: "Extensiones de tríceps con cable", series: 3, reps: 12 }
         ]
     },
     2: {
         day: "Día 2: Espalda y Bíceps",
         exercises: [
-            { name: "Dominadas", series: "4", reps: "6-10" },
-            { name: "Remo con barra", series: "3", reps: "8-12" },
-            { name: "Remo con mancuernas a un brazo", series: "3", reps: "10-12" },
-            { name: "Jalón al pecho", series: "3", reps: "12" },
-            { name: "Curl con barra", series: "3", reps: "10-12" },
-            { name: "Curl de bíceps con mancuernas alterno", series: "3", reps: "12" }
+            { name: "Dominadas", series: 4, reps: "6-10" },
+            { name: "Remo con barra", series: 3, reps: "8-12" },
+            { name: "Remo con mancuernas a un brazo", series: 3, reps: "10-12" },
+            { name: "Jalón al pecho", series: 3, reps: 12 },
+            { name: "Curl con barra", series: 3, reps: "10-12" },
+            { name: "Curl de bíceps con mancuernas alterno", series: 3, reps: 12 }
         ]
     },
     3: {
         day: "Día 3: Piernas y Abdomen",
         exercises: [
-            { name: "Sentadillas", series: "4", reps: "8-12" },
-            { name: "Prensa de pierna", series: "3", reps: "10-12" },
-            { name: "Peso muerto rumano", series: "3", reps: "10-12" },
-            { name: "Extensiones de cuádriceps", series: "3", reps: "12" },
-            { name: "Curl de pierna", series: "3", reps: "12" },
-            { name: "Elevación de talones (gemelos)", series: "4", reps: "15-20" },
-            { name: "Crunch abdominal", series: "3", reps: "20" },
-            { name: "Elevación de piernas", series: "3", reps: "15" }
+            { name: "Sentadillas", series: 4, reps: "8-12" },
+            { name: "Prensa de pierna", series: 3, reps: "10-12" },
+            { name: "Peso muerto rumano", series: 3, reps: "10-12" },
+            { name: "Extensiones de cuádriceps", series: 3, reps: 12 },
+            { name: "Curl de pierna", series: 3, reps: 12 },
+            { name: "Elevación de talones (gemelos)", series: 4, reps: "15-20" },
+            { name: "Crunch abdominal", series: 3, reps: 20 },
+            { name: "Elevación de piernas", series: 3, reps: 15 }
         ]
     },
     4: {
         day: "Día 4: Hombros y Trapecios",
         exercises: [
-            { name: "Press militar con barra", series: "4", reps: "8-12" },
-            { name: "Elevaciones laterales", series: "3", reps: "12" },
-            { name: "Elevaciones frontales", series: "3", reps: "12" },
-            { name: "Pájaros (elevaciones posteriores)", series: "3", reps: "12" },
-            { name: "Encogimientos de hombros con barra", series: "4", reps: "15" }
+            { name: "Press militar con barra", series: 4, reps: "8-12" },
+            { name: "Elevaciones laterales", series: 3, reps: 12 },
+            { name: "Elevaciones frontales", series: 3, reps: 12 },
+            { name: "Pájaros (elevaciones posteriores)", series: 3, reps: 12 },
+            { name: "Encogimientos de hombros con barra", series: 4, reps: 15 }
         ]
     }
 };
@@ -58,17 +58,17 @@ function updateRoutine(day) {
     exercisesTable.innerHTML = "";
 
     routineDay.exercises.forEach((exercise, index) => {
-        const reps = parseInt(exercise.reps.split('-')[1], 10); // Máximo número de repeticiones
+        const sets = exercise.series;
         const row = document.createElement("tr");
         row.innerHTML = `
             <td>${exercise.name}</td>
             <td>${exercise.series}</td>
             <td>${exercise.reps}</td>
             <td>
-                ${[...Array(reps).keys()].map(i => `<input type="number" id="weight-${index}-${i+1}" data-exercise="${index}" class="weight-input" placeholder="Set ${i+1}">`).join('')}
+                ${[...Array(sets).keys()].map(i => `<input type="number" id="weight-${index}-${i+1}" data-exercise="${index}" class="weight-input" placeholder="Set ${i+1}">`).join('')}
             </td>
             <td>
-                ${[...Array(reps).keys()].map(i => `<input type="number" id="reps-${index}-${i+1}" data-exercise="${index}" class="reps-input" placeholder="Set ${i+1}">`).join('')}
+                ${[...Array(sets).keys()].map(i => `<input type="number" id="reps-${index}-${i+1}" data-exercise="${index}" class="reps-input" placeholder="Set ${i+1}">`).join('')}
             </td>
         `;
         exercisesTable.appendChild(row);
